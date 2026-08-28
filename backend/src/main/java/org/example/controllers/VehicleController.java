5d1c9ae57843f8f1d48ad975a4f5837a7a282714
Sean Simpson <JediTiger@users.noreply.github.com>
2026-08-28 02:15:51 -0500

    VehicleController in progress

--- backend/src/main/java/org/example/controllers/VehicleController.java
+++ backend/src/main/java/org/example/controllers/VehicleController.java
@@ -2,5 +2,24 @@ package org.example.controllers;
 
 public class VehicleController {
 
+    // TODO: Needs Rest, mapping, origin and authorize annotations
 
+    public class VehicleController {
+
+        // TODO: Needs auto wired DAO
+
+        // TODO: Get users garage (can sort by year or make)
+
+        // TODO: Get public vehicles for public view
+
+        // TODO: Search vehicles by make or model
+
+        // TODO: Create vehicle
+
+        // TODO: Update vehicle
+
+        // TODO: Delete vehicle
+
+        // TODO: Special method for Admin view all
+    }
 }
