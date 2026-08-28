4a7ba36628dc3ddb00353fe802da16d77dd096dd
Sean Simpson <JediTiger@users.noreply.github.com>
2026-08-28 02:20:52 -0500

    VehicleController in progress

---
+++ backend/src/main/java/org/example/controllers/ServiceRecordController.java
@@ -0,0 +1,22 @@
+package org.example.controllers;
+
+public class ServiceRecordController {
+// TODO: Needs Rest, mapping, origin and authorize annotations
+
+// TODO: Needs auto wired DAO
+
+// TODO: Get users garage (can sort by year or make)
+
+// TODO: Get public vehicles for public view
+
+// TODO: Search vehicles by make or model
+
+// TODO: Create vehicle
+
+// TODO: Update vehicle
+
+// TODO: Delete vehicle
+
+// TODO: Special method for Admin view all
+
+}
--- backend/src/main/java/org/example/controllers/VehicleController.java
+++ backend/src/main/java/org/example/controllers/VehicleController.java
@@ -1,25 +1,41 @@
 package org.example.controllers;
 
+import org.example.daos.VehicleDao;
+import org.example.models.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+// Needs Rest, mapping, origin and authorize annotations
+@RestController
+@RequestMapping({ "/vehicles", "/api/vehicles" })
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
 public class VehicleController {
 
-    // TODO: Needs Rest, mapping, origin and authorize annotations
-
-    public class VehicleController {
-
-        // TODO: Needs auto wired DAO
-
-        // TODO: Get users garage (can sort by year or make)
-
-        // TODO: Get public vehicles for public view
-
-        // TODO: Search vehicles by make or model
-
-        // TODO: Create vehicle
-
-        // TODO: Update vehicle
-
-        // TODO: Delete vehicle
-
-        // TODO: Special method for Admin view all
+    // Needs auto wired DAO
+    @Autowired
+    private VehicleDao vehicleDao;
+    // TODO: Get users garage (can sort by year or make)
+    @GetMapping("/my")
+    public List<Vehicle> getMyVehicles(
+            Principal principal,
+            @RequestParam(defaultValue = "year") String sortBy,
+            @RequestParam(defaultValue = "desc") String direction) {
+        return vehicleDao.getByUsername(principal.getName(), sortBy, direction);
     }
-}
+    // TODO: Get public vehicles for public view
+
+    // TODO: Search vehicles by make or model
+
+    // TODO: Create vehicle
+
+    // TODO: Update vehicle
+
+    // TODO: Delete vehicle
+
+    // TODO: Special method for Admin view all
+    }
