4352c6244e9a020225912b8811cb2fab852fa9d0
Sean Simpson <JediTiger@users.noreply.github.com>
2026-08-28 01:15:53 -0500

    Vehicle model class in progress

--- backend/src/main/java/org/example/models/ServiceRecord.java
+++ backend/src/main/java/org/example/models/ServiceRecord.java
@@ -1,4 +1,21 @@
 package org.example.models;
 
+// Field will be id, vehicleId, serviceProvider, serviceName, description, cost, mileage, serviceDate, isPrivate
+/*
+    - id, vehicleId will be Long
+    - serviceProvider, serviceName, description, are all String
+    - cost will be double
+    - mileage will be integer
+    - serviceDate is date
+    - isPrivate is boolean
+ */
+
 public class ServiceRecord {
+    // TODO: Add vars for the fields
+
+    // TODO: Constructor(s)
+
+    // TODO: Getters & Setters
+
+
 }
--- backend/src/main/java/org/example/models/Vehicle.java
+++ backend/src/main/java/org/example/models/Vehicle.java
@@ -1,4 +1,19 @@
 package org.example.models;
 
+// Field will be id, username, make, model, year, isPrivate
+/*
+    - id will be Long
+    - username, make, model are all String
+    - year will be int
+    - isPrivate is boolean
+ */
+
 public class Vehicle {
+    // TODO: Add vars for the fields
+
+    // TODO: Constructor(s)
+
+    // TODO: Getters & Setters
+
+
 }
