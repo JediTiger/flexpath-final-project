0c14382ff1bbb5851abda50950037b8f5fd1f909
Sean Simpson <JediTiger@users.noreply.github.com>
2026-08-28 01:21:22 -0500

    Vehicle model class in progress

--- backend/src/main/java/org/example/models/Vehicle.java
+++ backend/src/main/java/org/example/models/Vehicle.java
@@ -9,11 +9,40 @@ package org.example.models;
  */
 
 public class Vehicle {
-    // TODO: Add vars for the fields
+    // Vars for the fields
+    private Long id;
+    private String username;
+    private String make;
+    private String model;
+    private int year;
+    private boolean isPrivate;
+    // Constructor(s)
+    public Vehicle() { }
 
-    // TODO: Constructor(s)
+    public Vehicle(Long id, String username, String make, String model, int year, boolean isPrivate) {
+        this.id = id;
+        this.username = username;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.isPrivate = isPrivate;
+    }
+    // Getters & Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    // TODO: Getters & Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
+    public String getMake() { return make; }
+    public void setMake(String make) { this.make = make; }
 
+    public String getModel() { return model; }
+    public void setModel(String model) { this.model = model; }
+
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+
+    public boolean isPrivate() { return isPrivate; }
+    public void setPrivate(boolean isPrivate) { this.isPrivate = isPrivate; }
 }
